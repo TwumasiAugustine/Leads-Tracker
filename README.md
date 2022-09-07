e68f06b544017cad2f332dcfcb25bbc49b089862
Twumasi Augustine <twumasiaugustine1997@gmail.com>
2022-09-07 01:45:23 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+# CHROME EXTENSIONS: Version 1.0.0
+
+Leads Tracker
+
+# ABOUT THIS EXTENSION
+
+This is a simple chrome extension that allows you to to copy and save active chrome tabs URLs
+
+You can also input the url manually by typing the url in the input field, click the Save Input Button to save
---
+++ index.js
@@ -0,0 +1,57 @@
+let myLeads = []
+const inputEl = document.getElementById("input-el")
+const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ul-el")
+const deleteBtn = document.getElementById("delete-btn")
+const saveTabBtn = document.getElementById("tab-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+
+
+if(leadsFromLocalStorage) {
+    myLeads = leadsFromLocalStorage
+    render(myLeads)
+}
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        // Making a list item clickable
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+
+    }
+    ulEl.innerHTML = listItems  
+}
+
+// Tab button
+saveTabBtn.addEventListener('click', function() {
+    // Grab the url of the current tab
+    chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
+    myLeads.push(tabs[0].url)
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    render(myLeads)
+    })
+    
+})
+
+
+
+// Save Input Button Function
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value)
+    inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+    renderLeads()
+
+})
+// Delete All Buttons Function
+deleteBtn.addEventListener("dblclick", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+}) 
