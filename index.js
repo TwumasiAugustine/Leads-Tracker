b346409430eb85e6c703814bb5db7c80e1bef166
Twumasi Augustine <twumasiaugustine1997@gmail.com>
2024-07-12 04:09:15 +0000

    Save tab functionality added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
+// const preview = document.getElementById('previewer');
 const inputBtn = document.getElementById('input-btn');
 const ulEl = document.getElementById('ul-el');
 const deleteBtn = document.getElementById('delete-btn');
@@ -7,59 +8,67 @@ const saveTabBtn = document.getElementById('tab-btn');
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'));
 
 if (leadsFromLocalStorage) {
-  myLeads = leadsFromLocalStorage;
-  render(myLeads);
+    myLeads = leadsFromLocalStorage;
+    render(myLeads);
 }
 
 function render(leads) {
-  let listItems = '';
-  for (let i = 0; i < leads.length; i++) {
-    // Making a list item clickable
-    listItems += `
-            <li>
-                <a target='_blank' href='${leads[i]}'>
-                    ${leads[i]}
-                </a>
-                <i class="remove-btn fa fa-trash" data-index="${i}" style="color: red; float: right; margin-right: 40px; cursor: pointer"; ></i>
-            </li>
-        `;
-    }
+    const listItems = leads.map((lead, index) => `
+        <li>
+            <a target='_blank' href='${lead}'>
+                ${lead}
+            </a>
+            <i class="remove-btn fa fa-trash" data-index="${index}" style="color: red; float: right; margin-right: 10px; cursor: pointer"; ></i>
+        </li>
+    `).join('');
     ulEl.innerHTML = listItems;
-    
-    // Add event listeners for each remove button
-    const removeButtons = document.querySelectorAll('.remove-btn');
-    removeButtons.forEach(function(btn) {
-        btn.addEventListener('click', function() {
-            const index = btn.getAttribute('data-index');
-            myLeads.splice(index, 1);
-            localStorage.setItem('myLeads', JSON.stringify(myLeads));
-            render(myLeads);
-        });
-    });
 }
+
+// Event delegation for remove buttons
+ulEl.addEventListener('click', function(event) {
+    if (event.target.classList.contains('remove-btn')) {
+        const index = event.target.getAttribute('data-index');
+        myLeads.splice(index, 1);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        render(myLeads);
+    }
+});
 
 // Tab button
 saveTabBtn.addEventListener('click', function() {
-  // Grab the url of the current tab
-    chrome.tabs.query({ active: true, currentWindow: true }, 
-    function(tabs) {
-    myLeads.push(tabs[0].url);
-    localStorage.setItem('myLeads', JSON.stringify(myLeads));
-    render(myLeads);
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem('myLeads', JSON.stringify(myLeads));
+        render(myLeads);
+
+        // Check if notifications are permitted
+        if (Notification.permission === 'granted') {
+            // Display a notification
+            new Notification('Link Saved', {
+                body: `You've successfully saved the link: ${tabs[0].url}`,
+                icon: 'link_icon.png' // Optional: add a link icon or any relevant icon
+            });
+        }
     });
 });
+
+// Link preview
+// inputEl.addEventListener('input', () => {
+//   preview.innerHTML = ': ' + inputEl.value
+//   console.log('typing:', inputEl.value);
+// });
 
 // Save Input Button Function
 inputBtn.addEventListener('click', function() {
     myLeads.push(inputEl.value);
     inputEl.value = '';
     localStorage.setItem('myLeads', JSON.stringify(myLeads));
-    render();
+    render(myLeads);
 });
 
 // Delete All Buttons Function
 deleteBtn.addEventListener('dblclick', function() {
-  localStorage.clear();
-  myLeads = [];
-  render(myLeads);
+    localStorage.clear();
+    myLeads = [];
+    render(myLeads);
 });
